d8ccd14fcc3e3e15cb473d5b5f368c9e82b5b187
Lorenzo <lorenzopica31@gmail.com>
2025-02-16 16:47:31 +0100

    add drag function

--- script.js
+++ script.js
@@ -17,7 +17,28 @@ class Ship{
         this.grid = document.getElementById(playerGrid);
         this.element = document.createElement('div');
         this.element.classList.add('ship');
+        this.element.draggable = true ;
+        this.element.setAttribute("data-length", "4");
+        this.element.addEventListener('dragstart', (e) => {
+            e.dataTransfer.setData('text/plain', JSON.stringify({ length, gridId }));
+        });
         this.grid.appendChild(this.element);
+    }
+}
+
+function dropShip(event) {
+    event.preventDefault();
+    const data = JSON.parse(event.dataTransfer.getData('text/plain'));
+    const cellIndex = parseInt(event.target.dataset.index);
+    const x = cellIndex % 10;
+    const y = Math.floor(cellIndex / 10);
+
+    if (x + data.length > 10) return;
+    
+    const shipElement = document.querySelector(`.ship[style*="left: ${data.x * 40}px;"][style*="top: ${data.y * 40}px;"]`);
+    if (shipElement) {
+        shipElement.style.left = `${x * 40}px`;
+        shipElement.style.top = `${y * 40}px`;
     }
 }
 
